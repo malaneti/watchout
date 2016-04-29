a61372578a515a0a53e7b1e9449db4337c0e83be
chris.ng93 <chris.ng93@gmail.com>
2016-04-29 15:04:12 -0700

    Created enemies

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,44 @@
 // start slingin' some d3 here.
+var boardWidth = 600;
+var boardHeight = 400;
+var svg = d3.select('.board')
+            .append('svg')
+            .attr('width', boardWidth)
+            .attr('height', boardHeight);
+
+var allEnemies = [1,1,1,1,2,2,2,2];
+
+var createEnemies = svg.selectAll('circle')
+                       .data(allEnemies)
+                       .enter()
+                       .append('circle')
+                       .attr('cx', function(d) {
+                        return d * 10;
+                       })
+                       .attr('cy', function(d) {
+                        return d * 10;
+                       })
+                       .attr('r', function(d) {
+                        return d * 10;
+                       })
+                       .attr('class', 'center')
+                       .attr('class', 'circles')
+                       .attr('position', 'absolute')
+                       .attr('left', boardWidth / 2)
+                       .attr('top', boardHeight / 2)
+                       .style('fill', 'white');
+
+var selectEnemies = svg.selectAll('circle');
+
+var moveEnemies = function() {
+  selectEnemies.transition()
+               .attr('cx', function() {
+                var random = Math.random() * 200;
+                return random;
+               })
+               .attr('cy', function() {
+                var random = Math.random() * 200;
+                return random;
+               })
+};
+setInterval(moveEnemies, 1000);
